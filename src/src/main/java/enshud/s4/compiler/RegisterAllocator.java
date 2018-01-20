93797aaab3c7634837aa01eccd73ca6d5ef9c110
kk-mats <k.mats.kk@gmail.com>
2018-01-21 03:26:28 +0900

    remove BasicBlock and ControlFlowGraph definition

--- src/src/main/java/enshud/s4/compiler/RegisterAllocator.java
+++ src/src/main/java/enshud/s4/compiler/RegisterAllocator.java
@@ -2,8 +2,14 @@ package enshud.s4.compiler;
 
 public class RegisterAllocator
 {
+	private ControlFlowGraph controlFlowGraph;
 	String [] register=new String[8];
 
+	public RegisterAllocator(final CASL casl)
+	{
+		controlFlowGraph=new ControlFlowGraph(casl);
+		System.out.print(controlFlowGraph.toString());
+	}
 
 
 }
